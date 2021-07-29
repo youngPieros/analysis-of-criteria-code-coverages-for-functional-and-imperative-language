90e3d87267e93b58b583e46918509dd37ee4e241
iman.moradi <iman.moradi@tapsi.cab>
2021-07-30 00:03:15 +0430

    Add configs to generate both valid and invalid network test cases

--- repository/networkSorting/testgenerator/config.py
+++ repository/networkSorting/testgenerator/config.py
@@ -1,7 +1,6 @@
-number_of_networks = 10
+number_of_valid_networks = 10
+number_of_invalid_networks = 4
 
 network_size = 6
 
-number_of_stages = 20  # maximum 105
-
-valid_network = True
+number_of_stages = 11  # maximum 105
--- repository/networkSorting/testgenerator/testcaseGenerator.py
+++ repository/networkSorting/testgenerator/testcaseGenerator.py
@@ -4,15 +4,15 @@ import string
 
 legal_network_chars = string.ascii_lowercase + string.ascii_uppercase
 
-
-for i in range(config.number_of_networks):
+number_of_networks = config.number_of_valid_networks + config.number_of_invalid_networks
+for i in range(number_of_networks):
     network = ['' for _ in range(config.network_size)]
     numbers = [i for i in range(config.network_size)]
     random.shuffle(numbers)
     for j in range(config.number_of_stages):
         network_pairs = [i for i in range(config.network_size)]
         random.shuffle(network_pairs)
-        if (config.valid_network):
+        if (i < config.number_of_valid_networks):
             for k in range(config.network_size):
                 if config.network_size % 2 == 1 and network_pairs[k] == config.network_size - 1:
                     network[k] += '-'
@@ -25,11 +25,12 @@ for i in range(config.number_of_networks):
             else:
                 for k in range(config.network_size):
                     network[network_pairs[k]] += legal_network_chars[k // 3]
-    print(config.network_size)
-    print(config.number_of_stages)
+    print(str(config.network_size) + " " + str(config.number_of_stages))
     for net in network:
-        print(net)
+        print(net, end=" ")
+    print()
     for number in numbers:
-        print(number)
+        print(number, end=" ")
+    print()
 print ("0 0")
 
