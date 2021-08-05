4e5267db05f798f6159955ae0a962ddf6d1e85c9
iman.moradi <iman.moradi@tapsi.cab>
2021-08-06 02:26:40 +0430

    Revive haskell binary search

--- repository/binarySearch/haskell/Main.hs
+++ repository/binarySearch/haskell/Main.hs
@@ -15,10 +15,23 @@ toInt :: String -> Int
 toInt x = read x :: Int
 
 parseInputs :: [String] -> (Int, [Int])
-parseInputs inputs = (toInt (head inputs), map toInt (tail inputs))
+parseInputs inputs = (toInt (head $ tail inputs), map toInt (tail $ tail inputs))
+
+loop :: Int -> (IO()) -> IO()
+loop 0 _ = return()
+loop n f = do
+  f
+  loop (n - 1) f
+
+searchNumber = do
+  args <- getLine
+  let inputs = parseInputs (words args)
+  let searchNumber = fst inputs
+  let collection = snd inputs
+  let index = binarySearch searchNumber collection
+  putStrLn (show index :: String)
+
 
 main = do
- args <- getLine
- let inputs = parseInputs  (words args)
- let index = binarySearch (fst inputs) (snd inputs)
- putStrLn ("index: " ++ (show index :: String))
+  collectionNumbers <- getLine
+  loop (toInt collectionNumbers) searchNumber
