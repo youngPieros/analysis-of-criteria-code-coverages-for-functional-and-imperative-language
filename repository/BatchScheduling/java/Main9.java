c6adddfdad02730e6a0e3349057142527fd35057
iman.moradi <iman.moradi@tapsi.cab>
2021-08-23 13:09:08 +0430

    Remove Java.11 instance sample code in batch scheduling code

--- repository/BatchScheduling/java/Main9.java
+++ repository/BatchScheduling/java/Main9.java
@@ -1,56 +1,59 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Main9 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int []a = new int[n];
-        int []b = new int[n];
-        int[]ans = new int[n];
-        int max = 0, j,ansmax =0;
-        int count = 0;
-        for(int i = 0; i < n ; i++) {
-            a[i] = s.nextInt();
-            if(a[i] > max)
-                max = a[i];
-            int next = s.nextInt();
-            count = count + next;
-            b[i] = next;
-            if(i > 0){
-                for(j = 0; j < i; j++) {
-                    if(a[i] == a[j])
-                        break;
-
-                }
-                //	System.out.println("count ="+ count);
-                //	System.out.println("max=" + max);
-                if(j == i) {
-                    ans[i] = count - max;
-                    if(ans[i] > ansmax){
-                        ansmax = ans[i];
-                        System.out.println(ans[i]);
-                    }
-                    else
-                        System.out.println(ansmax);
-                }
-
-                else
-                {
-                    // System.out.println(b[i]);
-                    System.out.println((ansmax+b[i]));
-                    ansmax = ansmax+b[i];
-
-                }
-            }
-            else{
-                if(b[i]>a[i])
-                    System.out.println((b[i]- a[i]));
-                else
-                    System.out.println("0");
-            }
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] D = new int[n];
+        int[] m = new int[n];
+        long[] t = new long[n];
+        for (int i = 0; i < n; i++) {
+            D[i] = in.nextInt();
+            m[i] = in.nextInt();
         }
-        s.close();
+        int index = 0;
+        long maxOvershot = 0;
+        for (int i = 1; i <= n; i++) {
+            int insertIndex = sort(D, m, t, i);
+            long time = 0;
+            if (insertIndex < index) {
+                time = t[index] + m[insertIndex] - m[index];
+            }
+            else {
+                if (index == 0) time = 0;
+                else time = t[index-1];
+            }
+            for (int j = index; j < i; j++) {
+                time += m[j];
+                t[j] = time;
+                if (Math.max(0, time - D[j]) > maxOvershot) {
+                    maxOvershot = Math.max(0, time - D[j]);
+                    index = j;
+                }
+            }
+            System.out.println(maxOvershot);
+        }
+    }
+
+    public static int sort(int[] D, int[] m, long[] t, int len) {
+        int currentD = D[len-1];
+        int currentM = m[len-1];
+        long currentT = t[len-1];
+        int i = len-2;
+        while (i >= 0 && D[i] > currentD) {
+            D[i+1] = D[i];
+            m[i+1] = m[i];
+            t[i+1] = t[i];
+            i--;
+        }
+        D[i+1] = currentD;
+        m[i+1] = currentM;
+        t[i+1] = currentT;
+        return i + 1;
     }
 }
