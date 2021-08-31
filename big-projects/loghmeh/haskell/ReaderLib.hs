b804dd418b34db6141377202f419e24d122e5955
iman.moradi <iman.moradi@tapsi.cab>
2021-09-01 00:20:49 +0430

    Change ReaderLib module

--- big-projects/loghmeh/haskell/ReaderLib.hs
+++ big-projects/loghmeh/haskell/ReaderLib.hs
@@ -1,25 +1,16 @@
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module ReaderLib
 ( Scanner(..)
 , bstr
 , line
-, int
-, times
-, pair
-, str
 , many
 ) where
 
 
-import Control.Applicative (liftA2)
-import Control.Monad (replicateM)
-import Control.Monad.State (State, evalState, get, gets, put)
-import qualified Data.Set as Set
+import Control.Monad.State (State, get, put)
 import qualified Data.ByteString.Lazy.Char8 as C
-import Data.Maybe (fromJust)
 
 
 type Scanner = State [C.ByteString]
@@ -29,18 +20,6 @@ bstr = get >>= \case s : ss -> put ss >> return s
 
 line :: Scanner String
 line = C.unpack . C.takeWhile (\c -> c /= '\n') <$> bstr
-
-int :: Scanner Int
-int = fst . fromJust . C.readInt <$> bstr
-
-times :: Int -> Scanner a -> Scanner [a]
-times = replicateM
-
-pair :: Scanner a -> Scanner b -> Scanner (a, b)
-pair = liftA2 (,)
-
-str :: Scanner String
-str = C.unpack <$> bstr
 
 many :: Scanner a -> Scanner [a]
 many s = get >>= \case [] -> return []; _ -> (:) <$> s <*> many s
