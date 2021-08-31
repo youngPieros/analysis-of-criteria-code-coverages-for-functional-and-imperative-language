f799807f9cb607cfb56751d338fb813628da2db5
iman.moradi <iman.moradi@tapsi.cab>
2021-08-31 20:13:17 +0430

    Add getRestaurant, findFood, getUserBasket and updateOrder in DataBase module

--- big-projects/loghmeh/haskell/DataBase.hs
+++ big-projects/loghmeh/haskell/DataBase.hs
@@ -1,9 +1,19 @@
 module DataBase
 ( DataBase(..)
 , getEmptyDataBase
+, createDB
+, getRestaurant
+, findFood
+, getUserBasket
+, updateOrder
 ) where
 
+
+import Data.List
+import Data.Maybe
+
 import Restaurant
+import Food
 import Order
 
 
@@ -12,5 +22,31 @@ data DataBase = DataBase{ restaurants :: [Restaurant]
                         , orders :: [Order]
                         } deriving (Show)
 
+
+
 getEmptyDataBase :: DataBase
 getEmptyDataBase = DataBase{restaurants=[], DataBase.orders=[]}
+
+getRestaurant :: DataBase -> String -> Restaurant
+getRestaurant db restaurantName
+    | Data.Maybe.isJust restaurant = Data.Maybe.fromJust restaurant
+    | otherwise = EmptyRestaurant
+    where
+        restaurant = Data.List.find (\restaurant -> (Restaurant.name :: Restaurant -> String) restaurant == restaurantName) (restaurants db)
+
+findFood :: DataBase -> String -> String -> Food
+findFood db restaurantName foodName = getFood restaurant foodName
+    where
+        restaurant = getRestaurant db restaurantName
+
+getUserBasket :: DataBase -> String -> Order
+getUserBasket db username
+    | Data.Maybe.isJust basket = Data.Maybe.fromJust basket
+    | otherwise = EmptyOrder
+    where
+        basket = Data.List.find (\b -> (user :: Order -> String) b == username) (orders db)
+
+updateOrder :: DataBase -> Order -> DataBase
+updateOrder db order = DataBase{restaurants=(restaurants db), orders=updatedOrders}
+    where
+        updatedOrders = order:(filter (\o -> o /= order) (orders db))
