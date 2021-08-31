d0f6afaddbf4b290bda6af5d5bf859f884990797
iman.moradi <iman.moradi@tapsi.cab>
2021-08-31 20:02:54 +0430

    Add Empty Food type and rewrite == function for it

--- big-projects/loghmeh/haskell/Food.hs
+++ big-projects/loghmeh/haskell/Food.hs
@@ -13,11 +13,14 @@ data Food = Food { name :: String
                  , description :: String
                  , popularity :: Double
                  , price :: Double
-                 } deriving (Show, Generic)
+                 } | EmptyFood deriving (Show, Generic)
 
 
 instance Eq Food where
- f1 == f2 = name f1 == name f2
+    EmptyFood == EmptyFood = True
+    EmptyFood == Food _ _ _ _ = False
+    Food _ _ _ _ == EmptyFood = False
+    f1 == f2 = name f1 == name f2
 
 instance ToJSON Food where
     toEncoding = genericToEncoding defaultOptions
