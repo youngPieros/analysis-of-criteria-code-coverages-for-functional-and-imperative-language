98ae558a9c27d2318c000524acdeac983a38b44e
iman.moradi <iman.moradi@tapsi.cab>
2021-09-01 11:31:09 +0430

    Change haskell_html_report_generator.py

--- resources/haskell/haskell_html_report_generator.py
+++ resources/haskell/haskell_html_report_generator.py
@@ -1,22 +1,16 @@
-import re
 import sys
 
-values = [{'name': 'expressions_used', 'total': '', 'percentage': ''},{'name': 'boolean_coverage', 'total': '', 'percentage': ''},{'name': 'guards', 'total': '', 'percentage': ''},{'name': 'if_conditions', 'total': '', 'percentage': ''},{'name': 'qualifiers', 'total': '', 'percentage': ''},{'name': 'alternatives_used', 'total': '', 'percentage': ''},{'name': 'local_declarations_used', 'total': '', 'percentage': ''},{'name': 'top-level_declarations', 'total': '', 'percentage': ''}]
 
-file = open('haskell_report.html',mode='r')
-file_content = file.read()
-file.close()
+values = [{'name': 'td.bar', 'replaced': 'td.bar2'}, {'name': 'table.bar', 'replaced': 'table.bar2'}, {'name': 'class="bar"', 'replaced': 'class="bar2"'}]
 
-total_regex = re.compile(r'(\d+/\d+)')
-percentage_regex = re.compile(r'(\d+)%')
 
-for i in range(8):
-	coverage = input()
-	values[i]['total'] = total_regex.search(coverage).group(1)
-	values[i]['percentage'] = percentage_regex.search(coverage).group(1)
-
-for value in values:
-	file_content = file_content.replace('$' + value['name'] + '_total', value['total'])
-	file_content = file_content.replace('$' + value['name'] + '_percentage', value['percentage'] + '%')
-
-print(file_content)
+filenames = sys.argv[1:]
+for filename in filenames:
+	file = open(filename, mode='r')
+	file_content = file.read()
+	file.close()
+	for value in values:
+		file_content = file_content.replace(value['name'], value['replaced'])
+	file = open(filename, mode='w')
+	file.write(file_content)
+	file.close()
