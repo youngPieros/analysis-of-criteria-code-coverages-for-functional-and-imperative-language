08f69085f76f3d217e8e1645dda724d3496b5ee0
iman.moradi <iman.moradi@tapsi.cab>
2021-09-03 02:25:22 +0430

    Add CommandArguments module with AddCourse parameters

---
+++ big-projects/bolbolestan/haskell/CommandArguments.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module CommandArguments
+( StartEndArgument(..)
+, ClassTimeArgument(..)
+, AddCourseArgument(..)
+) where
+
+
+import qualified Data.ByteString.Lazy.Char8 as C
+import Data.Maybe
+
+import Control.Applicative
+import Control.Monad
+import Data.Aeson
+
+
+
+data StartEndArgument = StartEndArgument { start :: String
+                                         , end :: String
+                                         } deriving(Show)
+
+data ClassTimeArgument = ClassTimeArgument { days :: [String]
+                                           , time :: String
+                                           } deriving(Show)
+
+data AddCourseArgument = AddCourseArgument { code :: String
+                                           , name :: String
+                                           , instructor :: String
+                                           , units :: Int
+                                           , classTime :: ClassTimeArgument
+                                           , examTime :: StartEndArgument
+                                           , capacity :: Int
+                                           , prerequisites :: [String]
+                                           } deriving(Show)
+
+
+
+
+
+
+instance FromJSON StartEndArgument where
+    parseJSON (Object v) = StartEndArgument <$> v .: "start" <*> v .: "end"
+
+instance FromJSON ClassTimeArgument where
+    parseJSON (Object v) = ClassTimeArgument <$> v .: "days" <*> v .: "time"
+
+instance FromJSON AddCourseArgument where
+    parseJSON (Object v) = AddCourseArgument <$> v .: "code" <*> v .: "name" <*> v .: "instructor" <*> v .: "units" <*> v .: "classTime" <*> v .: "examTime" <*> v .: "capacity" <*> v .: "prerequisites"
+
