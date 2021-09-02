5357e17aa5eb8e478ecfd14f74c87c60b15277ad
iman.moradi <iman.moradi@tapsi.cab>
2021-09-03 02:28:49 +0430

    Implement main handler module

---
+++ big-projects/bolbolestan/haskell/Handler.hs
@@ -0,0 +1,45 @@
+module Handler
+( run ) where
+
+import qualified Data.ByteString.Lazy.Char8 as C
+import Control.Monad.State (State, evalState)
+
+import DataBase
+import Command
+import CommandArguments
+import CommandParser
+import Response
+import Application
+
+
+
+type Scanner = State [C.ByteString]
+type SystemState = (DataBase, [Response])
+
+
+changeSystemState :: SystemState -> (DataBase, Response) -> SystemState
+changeSystemState systemState (database, response) = (database, snd systemState ++ [response])
+
+runScanner :: Scanner a -> C.ByteString -> a
+runScanner = runScannerWith C.lines
+
+runScannerWith :: (C.ByteString -> [C.ByteString]) -> Scanner a -> C.ByteString -> a
+runScannerWith t s = evalState s . t
+
+runScript :: Scanner String
+runScript = do
+    commands <- parseCommands
+    let database = getEmptyDataBase
+    let systemState = foldl (\ss command -> changeSystemState ss (runCommand command (fst ss))) (database, []) commands
+    let responses = (unlines $ filter (\r -> r /= "") $ map (\(Response r) -> r) (snd systemState))
+    return responses
+
+
+runCommand :: Command -> DataBase -> (DataBase, Response)
+runCommand command database = case command of
+    AddCourse args -> Application.addCourse database args
+    BadCommand -> (database, Response "bad command!!!")
+
+
+run :: IO ()
+run = C.interact $ runScanner (C.pack <$> runScript)
