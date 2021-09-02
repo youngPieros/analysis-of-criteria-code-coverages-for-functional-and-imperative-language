dec7dda2f9154c85dbaebb33df4351ddf50df5da
iman.moradi <iman.moradi@tapsi.cab>
2021-09-03 02:26:11 +0430

    Add ReaderLib module to project

---
+++ big-projects/bolbolestan/haskell/ReaderLib.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE LambdaCase #-}
+
+module ReaderLib
+( Scanner(..)
+, line
+, many
+) where
+
+
+import Control.Monad.State (State, get, put)
+import qualified Data.ByteString.Lazy.Char8 as C
+
+
+type Scanner = State [C.ByteString]
+
+bstr :: Scanner C.ByteString
+bstr = get >>= \case s : ss -> put ss >> return s
+
+line :: Scanner String
+line = C.unpack . C.takeWhile (\c -> c /= '\n') <$> bstr
+
+many :: Scanner a -> Scanner [a]
+many s = get >>= \case [] -> return []; _ -> (:) <$> s <*> many s
