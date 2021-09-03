91a789eb01ef48b4d32f65cb65c38cc5e93913cc
iman.moradi <iman.moradi@tapsi.cab>
2021-09-03 09:51:21 +0430

    Add students table in in data base and create and find student function in db

--- big-projects/bolbolestan/haskell/DataBase.hs
+++ big-projects/bolbolestan/haskell/DataBase.hs
@@ -3,6 +3,8 @@ module DataBase
 , getEmptyDataBase
 , addCourse
 , searchCourse
+, addStudent
+, findStudent
 ) where
 
 
@@ -10,22 +12,34 @@ import Data.List
 import Data.Maybe
 
 import Course
-
+import Student
 
 
 data DataBase = DataBase { courses :: [Course]
+                         , students :: [Student]
                          }
 
 
 
 getEmptyDataBase :: DataBase
-getEmptyDataBase = DataBase{courses=[]}
+getEmptyDataBase = DataBase{courses=[], students=[]}
 
 addCourse :: DataBase -> Course -> DataBase
-addCourse db course = DataBase{courses=(course:(courses db))}
+addCourse db course = DataBase{courses=(course:(courses db)), students=(students db)}
 
 searchCourse :: DataBase -> String -> [Course]
 searchCourse db courseCode = searchedCourses
     where
         searchedCourses = Data.List.filter (\c -> (code :: Course -> String) c == courseCode) (courses db)
+
+addStudent :: DataBase -> Student -> DataBase
+addStudent db student = DataBase{courses=(courses db), students=(student:(students db))}
+
+findStudent :: DataBase -> String -> Student
+findStudent db sid
+    | isJust student = fromJust student
+    | otherwise = NullStudent
+    where
+        student = Data.List.find (\s -> (studentId :: Student -> String) s == sid) (students db)
+
 
