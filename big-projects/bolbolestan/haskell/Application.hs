b7b22da84a495038a3111b17d462f0753a22975b
iman.moradi <iman.moradi@tapsi.cab>
2021-09-03 08:11:58 +0430

    Complete addCourse application

--- big-projects/bolbolestan/haskell/Application.hs
+++ big-projects/bolbolestan/haskell/Application.hs
@@ -6,7 +6,17 @@ module Application
 import DataBase
 import Response
 import CommandArguments
+import DataMapper
+import Course
+
+
 
 addCourse :: DataBase -> AddCourseArgument -> (DataBase, Response)
-addCourse db args = (db, Response "hiihihihihi")
+addCourse db args
+    | sameCourse == False = (DataBase.addCourse db course, Response "successful")
+    | otherwise = (db, Response "This course has conflict in class time with current courses")
+    where
+        sameCourse = elem course searchedCourses
+        searchedCourses = searchCourse db ((Course.code :: Course -> String) course)
+        course = toCourse args
 
