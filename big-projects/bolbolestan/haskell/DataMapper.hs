0250c5794a31735f820af55fd7619f4f46b35c43
iman.moradi <iman.moradi@tapsi.cab>
2021-09-03 09:51:41 +0430

    generate data mapper for student type

--- big-projects/bolbolestan/haskell/DataMapper.hs
+++ big-projects/bolbolestan/haskell/DataMapper.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module DataMapper
 ( toCourse
+, toStudent
 ) where
 
 
@@ -10,15 +13,16 @@ import ClassTime
 import DateTime
 import ExamTime
 import Course
+import Student
 import CommandArguments
 
 
 
 toCourse :: AddCourseArgument -> Course
-toCourse args = Course code name instructor units classTime examTime capacity prerequisites
+toCourse args = Course courseCode courseName instructor units classTime examTime capacity prerequisites
     where
-        code = (CommandArguments.code :: AddCourseArgument -> String) args
-        name = (CommandArguments.name :: AddCourseArgument -> String) args
+        courseCode = (code :: AddCourseArgument -> String) args
+        courseName = (name :: AddCourseArgument -> String) args
         instructor = (CommandArguments.instructor :: AddCourseArgument -> String) args
         units = (CommandArguments.units :: AddCourseArgument -> Int) args
         examTime = ExamTime (read startExam :: DateTime) (read endExam :: DateTime)
@@ -31,4 +35,11 @@ toCourse args = Course code name instructor units classTime examTime capacity pr
         ct = (CommandArguments.classTime :: AddCourseArgument -> ClassTimeArgument) args
         capacity = (CommandArguments.capacity :: AddCourseArgument -> Int) args
         prerequisites = (CommandArguments.prerequisites :: AddCourseArgument -> [String]) args
+
+toStudent :: AddStudentArgument -> Student
+toStudent args = Student sid studentName studentEnteredAt
+    where
+        sid = (studentId :: AddStudentArgument -> String) args
+        studentName = (name :: AddStudentArgument -> String) args
+        studentEnteredAt = (enteredAt :: AddStudentArgument -> Int) args
 
